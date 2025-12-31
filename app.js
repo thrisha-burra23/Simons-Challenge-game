7ff47d291ad1322b4124b9623086b42ab52cc6e3
Thrisha Burra <thrishagoud023@gmail.com>
2025-12-31 19:12:09 +0530

    basic setup like level btn and start and back for all 3 levels

---
+++ app.js
@@ -0,0 +1,98 @@
+const mainContainer = document.getElementById("mainContainer");
+document.addEventListener("DOMContentLoaded",createLevels);
+
+function createLevels() {
+    mainContainer.innerHTML = "";
+
+    const levelContainer = document.createElement("div");
+    const levelBtnContainer = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const easyLevelBtn = document.createElement("button");
+    const mediumLevelBtn = document.createElement("button");
+    const hardLevelBtn = document.createElement("button");
+
+    h1.textContent = "Select level";
+    easyLevelBtn.textContent = "Easy";
+    mediumLevelBtn.textContent = "Medium";
+    hardLevelBtn.textContent = "Hard";
+
+    levelContainer.classList.add("levelContainer");
+    levelBtnContainer.classList.add("levelBtnContainer");
+    h1.classList.add("mainHeading");
+    easyLevelBtn.classList.add("easyLevelBtn")
+    mediumLevelBtn.classList.add("mediumLevelBtn");
+    hardLevelBtn.classList.add("hardLevelBtn");
+
+    easyLevelBtn.addEventListener("click", handleEasyLevel);
+    mediumLevelBtn.addEventListener("click", handleMediumLevel);
+    hardLevelBtn.addEventListener("click", handleHardLevel);
+
+    levelBtnContainer.appendChild(easyLevelBtn);
+    levelBtnContainer.appendChild(mediumLevelBtn);
+    levelBtnContainer.appendChild(hardLevelBtn);
+
+    levelContainer.appendChild(h1);
+    levelContainer.appendChild(levelBtnContainer);
+
+    mainContainer.appendChild(levelContainer);
+
+}
+
+function btnCreate( handleLevel) {
+
+    const backBtn = document.createElement("button");
+    backBtn.className = "backBtn";
+    backBtn.textContent = "Back";
+    backBtn.addEventListener("click",createLevels);
+    backBtn.classList.add("backBtn");
+
+
+    const startBtn = document.createElement("button");
+    startBtn.textContent = "Start";
+    startBtn.classList.add("startBtn");
+    startBtn.addEventListener("click",handleLevel)
+
+    const btnContainer = document.createElement("div");
+    btnContainer.appendChild(backBtn);
+    btnContainer.appendChild(startBtn);
+
+   return btnContainer;
+
+}
+
+function handleEasyLevel() {
+    console.log("clicked easy level button");
+    mainContainer.innerHTML = "";
+
+
+    const btnGroup=btnCreate(handleEasyStart);
+    mainContainer.appendChild(btnGroup);
+}
+
+function handleMediumLevel() {
+    console.log("clicked medium level button");
+    mainContainer.innerHTML = "";
+
+     const btnGroup=btnCreate(handleMediumStart);
+    mainContainer.appendChild(btnGroup);
+}
+
+function handleHardLevel() {
+    console.log("clicked hard level button");
+    mainContainer.innerHTML = "";
+
+  const btnGroup=btnCreate(handleHardStart);
+    mainContainer.appendChild(btnGroup);
+}
+
+function handleEasyStart(){
+    console.log("Easy Level start clicked")
+}
+
+function handleMediumStart(){
+    console.log("Medium Level start clicked")
+}
+
+function handleHardStart(){
+    console.log("Hard Level start clicked")
+}
